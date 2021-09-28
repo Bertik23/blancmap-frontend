29bcf008d401155acd61379938ca3c42cb6ba4ac
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2021-09-28 22:22:09 +0200

    blanc map making

---
+++ src/views/MainApp.vue
@@ -0,0 +1,29 @@
+<template>
+    <main-layout>
+      <div class="grid grid-cols-6 h-full">
+        <div class="col-span-6">
+          <form>
+            <input type="text"><button type="submit" class="">Search</button>
+          </form>
+        </div>
+        <div class="col-span-5">
+          <LMap/>
+        </div>
+        <div class="w-52 min-w-52">
+          Here make it
+        </div>
+      </div>
+    </main-layout>
+</template>
+
+<script lang="ts">
+import MainLayout from '../layouts/Main.vue'
+import LMap from "../components/LMap.vue"
+
+export default {
+  components: {
+    MainLayout,
+    LMap
+  }
+}
+</script>
